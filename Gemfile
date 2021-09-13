082ae169fd85df37e9fe89f34c0cc6659b38f68e
Jibran Kalia <jibran.kalia@gmail.com>
2021-09-13 18:02:34 -0500

    close to finishing

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'https://rubygems.org'
+gem 'nokogiri'
+
+
+gem 'pry', '~> 0.13.1'
--- parse.rb
+++ parse.rb
@@ -1,13 +1,62 @@
+require 'nokogiri'
+require 'pry'
+require 'csv'
 
 
 class Transactions
-  def initialize(description, date, amount)
-    @description = description
-    @date = date
-    @amount = amount
+  def initialize
+    @@transactions = []
+  end
+
+  def push(transaction)
+    @@transactions << transaction
+  end
+
+  def print
+    @@transactions.each { |t| t.print }
+  end
+
+  def csv
+    csv_string = CSV.generate do |csv|
+      @@transactions.each do |t| 
+        csv << t.row
+      end
+    end
+
+    puts csv_string
   end
 end
 
-doc = File.open("accounts.html") { |f| Nokogiri::HTML(f) }
+class Transaction
+  attr_reader :description, :date, :amount, :balance
 
-#transactions > div > div > div > div > table > tbody > tr:nth-child(1)
+  def initialize(description, date, amount, balance)
+    @description = description
+    @date = date
+    @amount = amount
+    @balance = balance
+  end
+
+  def print
+    puts "#{date}, #{description}, #{amount}, #{balance}"
+  end
+
+  def row
+    [date, description, amount, balance]
+  end 
+end
+
+doc = File.open("accounts.html") { |f| Nokogiri::HTML5(f) }
+
+transactions = Transactions.new
+
+doc.css('[data-qe-automation="transactionRow"]').map do |transaction|
+  date = transaction.css('[data-qe-automation="date"]').text.strip
+  description = transaction.css('[data-qe-automation="description"]').text.strip
+  amount = transaction.css('[data-qe-automation="amount"]').text.strip
+  balance = transaction.css('[data-qe-automation="balance"]').text.strip
+  tran = Transaction.new(description, date, amount, balance)
+  transactions.push(tran)
+end
+
+transactions.csv
